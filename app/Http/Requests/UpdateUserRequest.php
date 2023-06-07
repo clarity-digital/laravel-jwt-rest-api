751741cfc86b73403f5b58e1727ce576d10d89df
Sjors Eveleens <45365598+devsjors@users.noreply.github.com>
2023-06-07 15:03:17 +0200

    Remove required rule from fields in UpdateUserRequest class (#1)

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -14,8 +14,8 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['string', 'max:255'],
+            'email' => ['string', 'email', 'max:255', 'unique:users'],
         ];
     }
 }
