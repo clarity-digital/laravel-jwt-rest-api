2fae2979f2f2e4a0dfe18b58705b0e7a05c43305
Lars Wolters <larstw98@gmail.com>
2023-06-15 10:07:40 +0200

    Remove verified middleware from user fetch route

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 require __DIR__ . '/auth.php';
 
-Route::middleware(['auth:api', 'verified'])->group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::get('/user', [UserController::class, 'show'])
         ->name('user.show');
+});
 
+Route::middleware(['auth:api', 'verified'])->group(function () {
     Route::patch('/user', [UserController::class, 'update'])
         ->name('user.update');
 
